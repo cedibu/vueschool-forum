eb059a5f4bb9cf36b48b88688f0ba8eb411eec6d
Cedric <cedric.bulloni@gmail.com>
2018-02-25 10:09:19 +0100

    VueSchool - Utility to Count Object Properties

--- src/components/PostListItem.vue
+++ src/components/PostListItem.vue
@@ -27,6 +27,8 @@
 </template>
 
 <script>
+  import { countObjectProperties } from '@/utils'
+
   export default {
     props: {
       post: {
@@ -39,7 +41,7 @@
         return this.$store.state.users[this.post.userId]
       },
       userPostsCount () {
-        return Object.keys(this.user.posts).length
+        return countObjectProperties(this.user.posts)
       }
     }
   }
