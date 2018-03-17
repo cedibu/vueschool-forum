9b260121cd55ed2dead1c8206b37cd40d6855a4f
Cedric <cedric.bulloni@gmail.com>
2018-03-17 14:45:08 +0100

    VueSchool - Improve the ThreadEditor

--- src/components/ThreadEditor.vue
+++ src/components/ThreadEditor.vue
@@ -19,7 +19,9 @@
 
     <div class="btn-group">
       <button @click.prevent="cancel" class="btn btn-ghost">Cancel</button>
-      <button class="btn btn-blue" type="submit" name="Publish">Publish </button>
+      <button class="btn btn-blue" type="submit" name="Publish">
+        {{isUpdate ? 'Update' : 'Publish'}}
+      </button>
     </div>
   </form>
 </template>
@@ -42,6 +44,12 @@
           title: this.title,
           text: this.text
         }
+      }
+    },
+
+    computed: {
+      isUpdate () {
+        return !!this.title
       }
     },
 
